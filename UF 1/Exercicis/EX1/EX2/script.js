30453acc38b0101bfa360f1273ae5bf75f7c88b6
sandro <sandro.antonucci.7e7@itb.cat>
2023-12-18 10:21:14 +0100

    update

---
+++ UF 1/Exercicis/EX1/EX2/script.js
@@ -0,0 +1,22 @@
+let title = document.getElementById('titulo');
+let helloWorld = document.getElementById('helloWorld');
+let byeWorld = document.getElementById('byeWorld');
+let importante = document.getElementById('importante');
+
+
+title.style.textTransform = "uppercase";
+title.style.fontFamily = "helvetica";
+title.style.width = "70%";
+title.style.backgroundColor = "lightblue";
+
+helloWorld.style.color = "red";
+helloWorld.style.backgroundColor = "lightgreen";
+helloWorld.style.fontFamily = "Arial";
+helloWorld.style.width = "70%";
+helloWorld.style.fontWeight = "700";
+
+byeWorld.style.color = "blue";
+
+importante.style.color = "yellow";
+importante.style.fontWeight = "bold";
+importante.style.backgroundColor = "Grey";
---
+++ UF 1/Exercicis/EX1/EX3/script.js
@@ -0,0 +1,6 @@
+let miDiv = document.getElementById('contenedor');
+let primerBoton = document.createElement('button');
+let textoPrimerBoton = document.createTextNode('1');
+primerBoton.appendText(textoPrimerBoton);
+
+miDiv.appendChild(primerBoton);
